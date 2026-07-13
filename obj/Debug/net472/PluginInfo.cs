b85b8c117920640c010ba267910985bb64c5e195
Noemi Panek <szynkorowskiyt@gmail.com>
2026-07-13 14:32:24 +0200

    Add files via upload

---
+++ obj/Debug/net472/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace Subnautica_Ultimate_modpack_core
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "Subnautica_Ultimate_modpack_core";
+        public const string PLUGIN_NAME = "Subnautica_Ultimate_modpack_core";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
